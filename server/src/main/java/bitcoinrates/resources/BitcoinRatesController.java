64fa23f46c71461018afc3701dd5a3b8b5ca9f4a
pruzicka <pavelruzicka@Pavels-MacBook-Pro.local>
2019-08-15 11:21:47 +0200

    Fix style

--- server/src/main/java/bitcoinrates/resources/BitcoinRatesController.java
+++ server/src/main/java/bitcoinrates/resources/BitcoinRatesController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,9 +20,10 @@ public class BitcoinRatesController {
 
     @CrossOrigin
     @GetMapping("/rates/{code}/")
-    public List<BitcoinRateTickDTO> getRateHistory(@PathVariable(value = "code") String code,
-                                                   @RequestParam("fromDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fromDate,
-                                                   @RequestParam("toDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime toDate) {
+    public List<BitcoinRateTickDTO> getRateHistory(
+            @PathVariable(value = "code") String code,
+            @RequestParam("fromDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fromDate,
+            @RequestParam("toDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime toDate) {
 
         List<BitcoinRate> bitcoinRates = bitcoinRatesService
                 .getBitcoinRates(code, fromDate, toDate);
